6837695770e3ad6cadec36f6271414cea76825fc
SanjaPopovic <sanja.popovic@stud.unibas.ch>
2020-04-21 19:01:29 +0200

    Add logStore code

--- app/src/main/python/main.py
+++ app/src/main/python/main.py
@@ -1,69 +1,34 @@
-from six.moves import input
-from crypto import create_keys, test_keys
-import sys
-
-CREATE = "create_keys"
-TEST = "test_keys"
-
-HMAC = "hmac"
-ED = "ED25519"
-
-options = [CREATE, TEST]
-types = [HMAC, ED]
+from SqLiteConnector import SqLiteConnector
 
 def main():
-	try:
-		run()
-	except Exception as e:
-		# print the exception and the line number at where it happened
-		#https://stackoverflow.com/questions/14519177/python-exception-handling-line-number/20264059
-		print("ERROR!!!!")
-		_, _, tb = sys.exc_info()
-		print(e)
-		print("At line number {}".format(tb.tb_lineno))
+    print("hey")
+    connector = SqLiteConnector()
+    connector.name_database('TestDatabase')
+    connector.start_database_connection()
+    connector.create_table('chat')
+    connector.insert_to_table('chat', '20:45', 'Peter', 'Hallo')
+    connector.insert_to_table('chat', '20:46', 'Max', 'Hallo')
+    connector.insert_to_table('chat', '20:47', 'Gustavo', 'Hallo')
+
+    connector.get_all_from_table('chat')
+    rows = connector.cursor.fetchall()
+
+    for row in rows:
+        print(row.format())
+
+    print("\n")
+    connector.search_in_table('chat', 'Peter')
+
+    rows = connector.cursor.fetchall()
+    print(rows)
+
+    connector.commit_changes()
+
+    connector.close_table('chat')
+
+    connector.close_database_connection()
 
 
-def run():
-	print("Avaliable commands: {}".format(options))
 
-	action = input()
-
-	if action not in options:
-		print("Invalid command, try again")
-		main()
-
-	if action == "create_keys":
-		print("Avaliable types: {}".format(types))
-		type_ = input()
-		if type_ not in types:
-			print("Invalid type, try again")
-			main()
-		if type_ == HMAC:
-			create_keys(hmac=True)
-		else:
-			create_keys(hmac=False)
-		run()
-
-	if action == "test_keys":
-		print("Avaliable types: {}".format(types))
-		type_ = input()
-		if type_ not in types:
-			print("Invalid type, try again")
-			main()
-		if type_ == HMAC:
-			test_keys(hmac=True)
-		else:
-			test_keys(hmac=False)
-		run()
-
-
-#def main():
-#    print("Enter your name, or an empty line to exit.")
-#    while True:
-#        try:
-#            name = input()
-#        except EOFError:
-#            break
-#        if not name:
-#            break
-#        print("Hello {}!".format(name))
+if __name__ == '__main__':
+    main()
